74cf18cc8a3ce0886f01438e5df28cb3eba55006
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2025-07-23 16:37:51 +0300

    feat(jwt-validation)

---
+++ backend/dotnet-services/UserRoleService/Cashier.Validation/Global/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Cashier.Validation.Interfaces;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Cashier.Core.Entities;
+global using Microsoft.AspNetCore.Identity;
